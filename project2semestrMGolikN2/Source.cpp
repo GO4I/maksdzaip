d2fedfaee32026b6a233d98acfb8bc9579200d61
Makxim <maxlivegame2@yandex.ru>
2020-03-14 13:10:15 +0300

    First Commit

--- project2semestrMGolikN2/Source.cpp
+++ project2semestrMGolikN2/Source.cpp
@@ -1,16 +1,32 @@
 #include "iostream"
 #include "iomanip"
 #include "math.h"
+#include "fstream"
+#include "string"
+
+using namespace std;
 
 //int main() {
 //
+//    void basicFileWrite();
+//
+//    std::ofstream outFile;
+//    outFile.open("results1.txt");
+//
 //    for (int i = 1; i < 25; i++) {
-//        for (int j = 0; j < i; j++)
-//            std::cout << 0;
+//        for (int j = 0; j < i; j++) {
+//
+//
+//          
+//           std::cout << 0;
+//           outFile << 0;
+//        }
 //        std::cout << std::endl;
+//        outFile << std::endl;
 //    }
 //
 //    std::cout << std::endl;
+//    outFile.close();
 //
 //    std::getchar();  
 //    std::getchar();  
@@ -31,18 +47,29 @@
         std::cout << "Enter deltax: ";  // дельта
         std::cin >> deltax;
 
+        std::ofstream outFile;
+
+        outFile.open("results2.txt");
+
         
         std::cout << "\tx\t\ty" << std::endl;
+        outFile << "\tx\t\ty" << std::endl;
 
         std::cout.precision(5);
         float y;
         float x = xmin;
-        while (x <= xmax) {
-            if (fabs(x < eps)) {
+        while (x < xmax) {
+            if (fabs(x-2) < eps) {
                 std::cout << "\t"
                     << x
                     << "\t\t Divide on zero or -deltax- is too low"
                     << std::endl;
+
+                outFile << "\t"
+                    << x
+                    << "\t\t Divide on zero or -deltax- is too low"
+                    << std::endl;
+
                 return 0;
             }
             else {
@@ -53,10 +80,17 @@
                     << "\t\t"
                     << y
                     << std::endl;
+
+                outFile << "\t"
+                    << x
+                    << "\t\t"
+                    << y
+                    << std::endl;
             }
             x = x + deltax;
         }
 
+        outFile.close();
         return 0;
 
     }
